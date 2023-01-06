6ec125d2964e9ecda120acf77ede867af0a7afc3
ytimesy <yukio.juna1224@gmail.com>
2023-01-06 18:39:01 +0900

    ルートアクセス時トップ画面表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()//JavaScriptファイルの処理が正常に動作しないことを防ぐため無効化
 require("@rails/activestorage").start()
 require("channels")
 
