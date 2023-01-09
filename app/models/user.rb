aa482d7c71c9e9b320583817e234a8d5d31b6cba
ytimesy <yukio.juna1224@gmail.com>
2023-01-09 20:48:41 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A[a-z\d]{6,}\z/i
+  validates :password,       presence: true, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
+  validates :nickname,       presence: true
+  validates :firstname,      presence: true
+  validates :lastname,       presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana,  presence: true
+  validates :birthday,       presence: true
 end
