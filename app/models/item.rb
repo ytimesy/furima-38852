c4a539d13989c9cb85014c80877c3e49e812cd72
ytimesy <yukio.juna1224@gmail.com>
2023-01-11 15:28:25 +0900

    item、userモデルでのバリデーション及びアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name,                presence: true
+  validates :content,             presence: true
+  validates :category_id,         presence: true
+  validates :status_id,           presence: true
+  validates :deliveryfee_type_id, presence: true
+  validates :prefecture_id,       presence: true
+  validates :delivery_when_id,    presence: true
 end
