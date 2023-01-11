702e2b1e66bfd66e7c87561c3520ad558334538a
ytimesy <yukio.juna1224@gmail.com>
2023-01-11 18:29:12 +0900

    ActiveHash表示入力実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :deliveryfee_type
+  belongs_to :prefecture
+  belongs_to :delivery_when_
+
   validates :name,                presence: true
   validates :content,             presence: true
-  validates :category_id,         presence: true
-  validates :status_id,           presence: true
-  validates :deliveryfee_type_id, presence: true
-  validates :prefecture_id,       presence: true
-  validates :delivery_when_id,    presence: true
+  validates :category_id,         presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id,           presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :deliveryfee_type_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,       presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_when_id,    presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :itemfee,             presence: true
 end
