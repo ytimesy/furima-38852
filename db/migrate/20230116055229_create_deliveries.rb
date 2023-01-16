e1992dfce6f48a6ad98b953bb7594943709337da
ytimesy <yukio.juna1224@gmail.com>
2023-01-16 15:11:08 +0900

    purchaseモデルとdeliveryモデル及びテーブル作成

---
+++ db/migrate/20230116055229_create_deliveries.rb
@@ -0,0 +1,14 @@
+class CreateDeliveries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :deliveries do |t|
+      t.string      :postcode,      null: false
+      t.integer     :prefecture_id, null: false
+      t.string      :city,          null: false
+      t.string      :address,       null: false
+      t.string      :building
+      t.string      :tel,           null: false
+      t.references  :purchases,     null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
